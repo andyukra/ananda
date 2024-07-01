56a91307460e6dfa8056ac41a10f40a384c7f612
andyukra <andyguachin87@gmail.com>
2024-07-01 11:17:11 -0300

    fix cards height

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,8 +8,8 @@
     import FaStar from 'svelte-icons/fa/FaStar.svelte';
 </script>
 
-<article class="w-full flex flex-col justify-between items-center h-[75dvh] hover:scale-95">
-    <img class="object-cover w-full rounded-t-xl shadow-lg h-[90%]" src={src} alt={title}>
+<article class="w-full flex flex-col justify-between items-center h-[75vh] hover:scale-95">
+    <img class="object-cover w-full rounded-t-xl shadow-lg" src={src} alt={title}>
     <div class="w-full shadow-lg rounded-lg divide-green-300 divide-y-8">
         <h1 class="w-full text-white text-center text-lg font-bold p-4 bg-slate-800 rounded-bl-lg rounded-br-lg">{title}</h1>
         {#if opts}
