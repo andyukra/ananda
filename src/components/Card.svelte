790e58363a9ca44efa0d38678fcce1bfbe31a52b
3lAndY <andyguachin87@gmail.com>
2024-10-29 13:18:19 -0300

    little ui fixes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -11,7 +11,7 @@
 <article class="w-full flex flex-col justify-between items-center h-[75vh] lg:hover:scale-95">
     <img class="object-cover w-full rounded-t-xl shadow-lg" src={src} alt={title}>
     <div class="w-full shadow-lg rounded-lg divide-green-300 divide-y-8">
-        <h1 class="w-full text-white text-center text-lg font-bold p-4 bg-slate-800 rounded-bl-lg rounded-br-lg">{title}</h1>
+        <h2 class="w-full text-white text-center text-sm font-bold md:p-4 p-2 bg-slate-800 rounded-bl-lg rounded-br-lg">{title}</h2>
         {#if opts}
             <div class="p-4 bg-slate-800 rounded-b-lg flex justify-around items-center">
                 <div class="size-5 text-slate-400 hover:text-white cursor-pointer">
@@ -34,10 +34,9 @@
         transition: 0.3s;
     }
     img {
-        max-height: max-content;
-        height: 90%;
+        height: 590px;
     }
-    h1 {
+    h2 {
         font-family: "Quicksand", sans-serif;
     }
 </style>
